1c3ac5e03ec485eaa1854873b7a7da6d5787305c
peytonc <chad.e.peyton@gmail.com>
2017-02-01 14:07:07 -0700

    fit

--- src/minijava/Fitness.java
+++ src/minijava/Fitness.java
@@ -1,29 +1,34 @@
 package minijava;
 
 public class Fitness implements Comparable<Fitness> {
+	public double fit;
 	public long difference;
 	public long speed;
 	public int size;
 	
 	public Fitness() {
+		fit = Double.MAX_VALUE;
 		difference = Long.MAX_VALUE;
 		speed = Integer.MAX_VALUE;
 		size = Integer.MAX_VALUE;
 	}
 	
 	public String toString() {
-		return "Fitness{difference=" + difference + ",size=" + size + ",speed=" + speed + "}";
+		return "Fitness{"fit=" + fit + ",difference=" + difference + ",size=" + size + ",speed=" + speed + "}";
 	}
 	
 	@Override
 	public int compareTo(Fitness fitness) {
-		int compare = Long.compare(difference, fitness.difference);
+int compare = Double.compare(fit, fitness.fit);
+		if(compare == 0) {
+		compare = Long.compare(difference, fitness.difference);
 		if(compare == 0) {
 			compare = Integer.compare(size, fitness.size);
 			if(compare == 0) {
 				compare = Long.compare(speed, fitness.speed);
 			}
 		}
+	}
 		return compare;
 	}
 }
